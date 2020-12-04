e75929f46a9e479fde5f5987f177553ba4f556ca
chisevew <chisevew@gmail.com>
2020-12-04 15:57:57 +0900

    更新

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Todo
+
+
+class TodoForm(forms.ModelForm):
+
+    class Meta:
+        model = Todo
+        fields = ['task', 'category', 'memo', 'completed', 'start_day', 'end_day', 'start_time','end_time']
+        widgets = {
+                    'task': forms.TextInput(),
+                    'memo': forms.Textarea(attrs={'rows':4}),
+                    'completed':forms.CheckboxInput(),
+                    'category':forms.Select(attrs={'choices':'CATEGORY_CHOICES'}),
+                    'start_day':forms.DateInput(attrs={'type': 'date',"format": "yyyy/mm/dd/",}),
+                    'end_day':forms.DateInput(attrs={'type': 'date', "format": "yyyy/mm/dd/",}),
+                    'start_time':forms.TimeInput(attrs={'type': 'time',"format": "hh:mm",}),
+                    'end_time':forms.TimeInput(attrs={'type': 'time',"format": "hh:mm"}),
+        }
